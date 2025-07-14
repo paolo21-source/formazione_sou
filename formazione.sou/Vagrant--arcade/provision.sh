e5e58bae875629ebb73239c620bcb5079536fefb
paolo21-source <paolo.astorino@sourcesense.com>
2025-07-14 12:26:50 +0200

    Delete formazione.sou directory

--- formazione.sou/Vagrant--arcade/provision.sh
+++
@@ -1,8 +0,0 @@
-bash 
-#!/bin/bash
-apt-get update
-apt-get install -y apache2 
-systemctl start apache2 
-mkdir -p /var/www/html
-cp -r /vagrant/site/. /var/www/html/
-chown -R www-data:www-data /var/www/html
